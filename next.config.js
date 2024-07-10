f13f5f540af3b76984c24f0d6f31511c894e6a04
mertdikbiyik <dikbykmert@gmail.com>
2024-07-10 03:52:07 +0300

    fixed vol2

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/dikbiyik-webapp",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
-    images: {
-        unoptimized: true,
-    },
 };
 
 module.exports = nextConfig;
